c84d33c3cce33981ad1b33217354b6c49e19adf1
Paul Moore <pdmoore@gmail.com>
2020-10-27 19:51:43 -0700

    mob next [ci-skip]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-fn fizzbuzz (input: u64) -> String {
-    "0".to_string()
+fn fizzbuzz(input: u64) -> String {
+    if 3 == input {
+        "Fizz".to_string()
+    } else {
+        input.to_string()
+    }
 }
 
 fn main() {
@@ -7,10 +11,15 @@ fn main() {
 }
 
 #[cfg(test)]
-mod tests {    
+mod tests {
     use super::*;
     #[test]
     fn fizzbuzz_1_test() {
         assert_eq!(fizzbuzz(1), 1.to_string());
+    }
+
+    #[test]
+    fn fizzbuzz_3() {
+        assert_eq!(fizzbuzz(3), "Fizz");
     }
 }
