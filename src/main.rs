73b9b9e64ed2def5ee8cab3977e5d6522863903c
Yves Dorfsman <yves@zioup.com>
2020-10-27 20:57:57 -0600

    mob next [ci-skip]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 fn fizzbuzz(input: u64) -> String {
-    if 3 == input {
+    if input % 3 == 0 {
         "Fizz".to_string()
+    } else if input % 5 == 0 {
+        "Buzz".to_string()
     } else {
         input.to_string()
     }
@@ -14,12 +16,19 @@ fn main() {
 mod tests {
     use super::*;
     #[test]
-    fn fizzbuzz_1_test() {
+    fn fizzbuzz_1() {
         assert_eq!(fizzbuzz(1), 1.to_string());
     }
 
     #[test]
-    fn fizzbuzz_3() {
+    fn fizzbuzz_multiple_of_3() {
         assert_eq!(fizzbuzz(3), "Fizz");
+        assert_eq!(fizzbuzz(6), "Fizz");
+        assert_eq!(fizzbuzz(9), "Fizz");
+    }
+    #[test]
+    fn fizzbuzz_multiple_of_5() {
+        assert_eq!(fizzbuzz(5), "Buzz");
+        assert_eq!(fizzbuzz(10), "Buzz");
     }
 }
